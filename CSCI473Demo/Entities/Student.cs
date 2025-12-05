faf01dd7c10d8da1708095cf7837b0f962280dc7
Larry Schoeneman <53457003+lschoeneman1@users.noreply.github.com>
2025-12-05 00:43:11 +0000

    initial

---
+++ CSCI473Demo/Entities/Student.cs
@@ -0,0 +1,9 @@
+namespace Entities;
+
+public class Student
+{
+    public string FirstName {get;set;} = string.Empty;
+    public string LastName {get;set; }= string.Empty;
+    public string Email {get;set; }= string.Empty;
+    public int Id  {get;set; }
+}
